a7a02e1ff49b56e9151cbf4a55ca893773f89571
David Borelli <david_borelli@live.com>
2022-08-18 12:45:27 -0300

    feat: adjust A

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import styled from 'styled-components';
 import { A } from './components/atoms/A';
 import { themes, ThemeOptionInitial, Theme } from './components/_settings/Theme/defaults';
 
 import StyleProvider from './components/_settings/Theme/ThemeProvider';
 
-export { A, themes, ThemeOptionInitial, Theme, StyleProvider };
+export { A, themes, ThemeOptionInitial, Theme, StyleProvider, styled };
