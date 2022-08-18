38a1ffd2f335a3b29380a2245025fb8870563520
David Borelli <david_borelli@live.com>
2022-08-18 12:21:56 -0300

    feat: adjust A component

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { A } from './components/atoms/A';
 import { themes, ThemeOptionInitial, Theme } from './components/_settings/Theme/defaults';
 
-export * as ThemeProvider from './components/_settings/Theme/ThemeProvider';
+import StyleProvider from './components/_settings/Theme/ThemeProvider';
 
-export { A, themes, ThemeOptionInitial, Theme };
+export { A, themes, ThemeOptionInitial, Theme, StyleProvider };
