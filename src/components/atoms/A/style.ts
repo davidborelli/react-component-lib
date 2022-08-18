72f8929c53e15fb0e90ca59d485150b0a6a5af29
David Borelli <david_borelli@live.com>
2022-08-18 00:16:03 -0300

    feat: adjust A component

--- src/components/atoms/A/style.ts
+++ src/components/atoms/A/style.ts
@@ -8,6 +8,9 @@ export const Link = styled.a<AStyleProps>`
   transition: background-color 600ms ease, transform 200ms ease, box-shadow 400ms ease-out;
   word-break: break-word;
   transition: color 250ms ease-in-out;
+  text-decoration: underline;
+  color: red;
+  font-size: 23px;
 
   &.is--underline {
     border-bottom-width: 1px;
