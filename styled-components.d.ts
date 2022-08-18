cf24dbcbf9587aa8438a6e90c1f274805d1c7ce0
David Borelli <david_borelli@live.com>
2022-08-18 11:40:54 -0300

    feat: adjust A component

--- styled-components.d.ts
+++ styled-components.d.ts
@@ -1,4 +1,4 @@
-import { Theme } from 'components/_settings/Theme/defaults';
+import { Theme } from './src/components/_settings/Theme/defaults';
 
 declare module 'styled-components' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
